7b2be8028bf4711345b0bac02dc56a309680c409
Madderstone <anthony.issa@hotmail.fr>
2023-03-25 16:06:15 +0100

    Added doc

--- notifications/src/telegram.js
+++ notifications/src/telegram.js
@@ -2,6 +2,13 @@ const fetch = require("node-fetch");
 
 const tokenRegex = /[0-9]+:[A-Za-z0-9]+/i;
 
+/**
+ * Send a Telegram message to a specific chat id.
+ * @param {string} token Token of the bot.
+ * @param {string} chatId Id of the chat to send the message to.
+ * @param {string} message Message to send.
+ * @returns {Promise<Response>} Response from the Telegram API.
+ */
 const sendTelegramNotification = async (
   token,
   chatId,
