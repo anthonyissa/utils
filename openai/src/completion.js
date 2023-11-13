6311bf17ab35d058c4ff490796555ad594ffd687
Madderstone <anthony.issa@hotmail.fr>
2023-11-13 22:17:27 +0100

    updated doc

--- openai/src/completion.js
+++ openai/src/completion.js
@@ -5,9 +5,7 @@ const { openai } = require("./index.js");
  * Send a prompt to the OpenAI API and return the result.
  * @param {string} model - The model to use for the API.
  * @param {string} prompt - The prompt to send to the API.
- * @param {number} max_tokens - The maximum number of tokens to generate.
- * @param {number} temperature - The sampling temperature.
- * @param {Object} args - The arguments to pass to the API.
+ * @param {Object} args - The arguments to pass to the API. See https://platform.openai.com/docs/api-reference
  */
 const completion = async ({ model, prompt, ...args }) => {
   const completion = await openai.chat.completions.create({
