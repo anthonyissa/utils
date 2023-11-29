e4301279191c112116f49488a9071db1446b952f
Madderstone <anthony.issa@hotmail.fr>
2023-11-29 17:42:16 +0100

    fixed openai

---
+++ openai/index.js
@@ -0,0 +1,9 @@
+const completion = require("./src/completion.js");
+const index = require("./src/index.js");
+const types = require("./src/types.js");
+
+module.exports = {
+  completion,
+  index,
+  types,
+};
